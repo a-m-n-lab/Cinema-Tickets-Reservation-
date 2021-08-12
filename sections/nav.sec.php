238d59a5487f487f6286de094638db9cfabc9c15
Ana-Maria Naros <72402281+a-m-n-lab@users.noreply.github.com>
2021-08-12 15:32:55 +0300

    Update nav.sec.php

--- sections/nav.sec.php
+++ sections/nav.sec.php
@@ -3,16 +3,16 @@
     <div class="navbar-header">
       <a class="navbar-brand" href="index.php">CINEMA</a>
     </div>
-
-
-
+      
+    
+    
     <ul class="nav navbar-nav">
-      <?php
+      <?php 
       if ($language == "romana") {
-
+         
        ?>
       <li class="active"><a href="index.php">Acasa</a></li>
-
+                
                 <li class="nav-item">
                     <a href="filme.php" class="nav-link active">Filme</a>
                 </li>
@@ -25,14 +25,14 @@
                 <li class="nav-item">
                     <a href="films.php" class="nav-link active">Franceza</a>
                 </li>
-          <?php } ?>
+          <?php } ?>     
 
-<?php
+<?php 
       if ($language == "engleza") {
-
+        
        ?>
       <li class="active"><a href="indexeng.php">Home</a></li>
-
+               
                 <li class="nav-item">
                     <a href="movies.php" class="nav-link active">Movies</a>
                 </li>
@@ -42,13 +42,13 @@
                 <li class="nav-item">
                     <a href="filme.php" class="nav-link active">Romanian</a>
                 </li>
-
+                
                 <li class="nav-item">
                     <a href="films.php" class="nav-link active">French</a>
                 </li>
-          <?php } ?>
+          <?php } ?> 
 
-          <?php
+          <?php 
       if ($language == "franceza") {
        ?>
       <li class="active"><a href="indexfr.php">Page d'accueil
@@ -66,22 +66,23 @@
                     <a href="movies.php" class="nav-link active">
                         Anglais</a>
                 </li>
-
-          <?php } ?>
+                
+          <?php } ?> 
     </ul>
      <ul class="nav navbar-nav navbar-right">
-
+      
       <?php if (isset($_SESSION['loggedin']) && $_SESSION['loggedin']) {
           if (isset($_SESSION['accountType']) && $_SESSION['accountType'] == 'admin') {
              echo '<li><a href="controlPanel.php"><span "></span> Control Panel</a></li>';//control panel apare doar daca esti admin
           }
         }
       if ($language == "romana") {
-
-
+         
+      
       if (isset($_SESSION['loggedin']) && $_SESSION['loggedin']) {
-
+          
           echo '
+          <li><a href="cos_ro.php"> Cos</a></li>
           <li><a href="logout.php"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>';//apare doar daca esti logat
       }else {
         echo '<li><a href="registration.html"><span class="glyphicon glyphicon-user"></span>Inregistrare</a></li>
@@ -90,13 +91,14 @@
 
       } ?>
 
-      <?php
+      <?php 
       if ($language == "engleza") {
-
-
+         
+      
       if (isset($_SESSION['loggedin']) && $_SESSION['loggedin']) {
-
+          
           echo '
+          <li><a href="cos.php">Cart</a></li>
           <li><a href="logout.php"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>';//apare doar daca esti logat
       }else {
         echo '<li><a href="registration.html"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
@@ -105,14 +107,16 @@
 
       } ?>
 
-      <?php
+      <?php 
       if ($language == "franceza") {
-
-
+         
+      
       if (isset($_SESSION['loggedin']) && $_SESSION['loggedin']) {
-
+          
           echo '
+          <li><a href="cos_fr.php"></span>Panier</a></li>
           <li><a href="logout.php"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>';//apare doar daca esti logat
+
       }else {
         echo '<li><a href="registration.html"><span class="glyphicon glyphicon-user"></span>Créer un compte</a></li>
       <li><a href="login.html"><span class="glyphicon glyphicon-log-in"></span>Se connecter</a></li>'; //apar in orice alt caz
